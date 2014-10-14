b5dcc27a1cf4ddf8478d731269ba27e7602c1c53
Yasith Vidanaarachchi <yasith@tuxv.net>
2014-10-14 17:13:39 -0400

    Basic app with buttons

--- CarController/app/src/main/java/ca/utoronto/utm/capstone2014/carcontroller/MainActivity.java
+++ CarController/app/src/main/java/ca/utoronto/utm/capstone2014/carcontroller/MainActivity.java
@@ -2,35 +2,36 @@ package ca.utoronto.utm.capstone2014.carcontroller;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 
 public class MainActivity extends Activity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
     }
 
+    @OnClick(R.id.forward)
+    public void forward() {
+        Log.d(TAG, "Forward");
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    @OnClick(R.id.reverse)
+    public void reverse() {
+        Log.d(TAG, "Reverse");
     }
 }
