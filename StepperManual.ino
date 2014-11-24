eefba05a0cfa7f8444c2e77436ce12a11f22f3b0
Yasith Vidanaarachchi <yasith.vidanaarachchi@gmail.com>
2014-11-23 20:36:57 -0500

    Update StepperManual.ino

--- StepperManual.ino
+++ StepperManual.ino
@@ -1,29 +1,58 @@
+#include <Servo.h>
 
 #define STEP 32
+#define TURN 16
+
+#define UP 135
+#define DOWN 90
 
 struct Stepper {
   int step;
   int dir;
 };
 
-struct Stepper right = {9, 8};
+struct Stepper right = {13, 12};
 struct Stepper left = {11, 10};
+int m = 1;
+
+Servo servo;
 
 void setup() {               
+  
+  Serial.begin(115200);
   
   pinMode(right.step, OUTPUT);
   pinMode(right.dir, OUTPUT);       
   pinMode(left.step, OUTPUT);
   pinMode(left.dir, OUTPUT);
   
-  //servo.attach(6);
-  //servo.write(UP);
+  servo.attach(10);
+  servo.write(UP);
 }
 
 
 void loop() {
-  move(1, 1, 8);
-  move(-1, -1, 8);
+  
+  if(Serial.available()) {
+    char in = Serial.read();
+   
+    if(in == 'l'){
+      move(1, -1, TURN);
+    } 
+    if(in == 'r'){
+      move(-1, 1, TURN);
+    }
+    if(in == 'f'){
+      move(1, 1, 1);
+    }
+    if(in == 'b'){
+      move(-1, -1, 1);  
+    }
+    if(in == 'm'){
+      servo.write(m == 1 ? DOWN : UP);
+      m = m == 1 ? 0 : 1;  
+    }
+  }
 }
 
 void move(int l, int r, int steps){
