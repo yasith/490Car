b5e56be13e9c6e7fabf11b6b0c9461e8af492e98
Yasith Vidanaarachchi <yasith.vidanaarachchi@gmail.com>
2014-11-23 21:20:35 -0500

    Function working

--- Stepper.ino
+++ Stepper.ino
@@ -14,25 +14,36 @@ void setup() {
 }
 
 void loop() {
-    digitalWrite(10, HIGH);
 
-    for(int i = 0; i < 32 * 16; i++) {
-      digitalWrite(11, HIGH);
-      delay(10);
-      digitalWrite(11, LOW);
-      delay(10);  
-    }
+    move(1, 1, 32 * 16);
     
     delay(1000);
     
     digitalWrite(10, LOW);
   
     for(int i = 0; i < 32 * 16; i++) {
-      digitalWrite(11, HIGH);
+      digitalWrite(right.step, HIGH);
       delay(10);
-      digitalWrite(11, LOW);
+      digitalWrite(right.step, LOW);
       delay(10);  
     }
   
     delay(1000);
+}
+
+void move(int r, int l, int step) {
+
+  if(r > 0) {
+    digitalWrite(right.dir, HIGH);  
+  } else {
+    digitalWrite(right.dir, LOW);
+  }
+  
+   for(int i = 0; i < step; i++) {
+      digitalWrite(right.step, HIGH);
+      delay(10);
+      digitalWrite(right.step, LOW);
+      delay(10);  
+    }
+  
 }
